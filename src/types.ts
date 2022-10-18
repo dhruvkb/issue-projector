7d2d5624e95c449828736294f08824342991c002
Dhruv Bhanushali <dhruvkb@users.noreply.github.com>
2022-10-18 21:34:52 +0000

    Deploy

---
+++ src/types.ts
@@ -0,0 +1,24 @@
+/**
+ * Reduced form of a GitHub issue
+ */
+export interface Issue {
+  id: number
+  title: string
+  isPullRequest: boolean
+}
+
+/**
+ * Error returned from the GitHub API
+ */
+export interface Error {
+  resource: string
+  code: string
+  field: string
+  message: string
+}
+
+export interface Outcome<T> {
+  isSuccessful: boolean
+  data?: T
+  errors: Array<string>
+}
